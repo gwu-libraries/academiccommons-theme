28b97f6223bb37039f756ce80dcef07a13721cbf
Max Starkenburg <maxstarkenburg@gmail.com>
2018-07-16 15:22:11 -0400

    Add sticky navbar functionality back in. Was in script.js which has been unreferenced. Added as plain jQuery document.ready instead of as Drupal Behaviors because I couldn't yet figure out how to prevent variable and initial conditional setup from running several times.

---
+++ lai/js/build/sticky-nav.js
@@ -0,0 +1,22 @@
+(function ($) {
+  $(document).ready(function(event) {
+    var navBar = $("#nav-bar");
+    // Fixed position menus after scrolling down past their location on the page
+    if ($(window).width() > 640) {
+      var toolbarTabHeight = $("#toolbar-administration .toolbar-tab:first").outerHeight() || 0;
+      var toolbarTrayHeight = $("#toolbar-administration .toolbar-tray:first").outerHeight() || 0;
+      var toolbarHeight = toolbarTabHeight + toolbarTrayHeight; // for some reason $("#toolbar-administration").outerHeight() is always returning 0 for me
+      var navBarOffset = navBar.offset().top;
+      $(document).on("scroll", function() {
+        if ($(document).scrollTop() > navBarOffset - toolbarHeight) {
+          navBar.addClass("sticky");
+          navBar.css({
+            "top" : toolbarHeight + "px",
+          });
+        } else {
+          navBar.removeClass("sticky");
+        }
+      });
+    }
+  });
+})(jQuery);
