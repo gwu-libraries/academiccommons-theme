7a4ddb8300056b7e227afeb20cc6fffae13708b8
Max Starkenburg <maxstarkenburg@gmail.com>
2018-07-17 19:01:01 -0400

    Adding styling so that it's clearer what fellowship filters one has already selected.

--- lai/js/build/exposed-filters-overrides.js
+++ lai/js/build/exposed-filters-overrides.js
@@ -26,6 +26,26 @@
 	  $(this).removeClass("focused-child");
 	}
       });
+
+      // Add class to ancestors of "input:checked" elements to indicate that something there was selected
+      function checkedChildren() {
+        var withCheckedChildren = $(".find-fellowships .checked-children");
+        withCheckedChildren.find("legend").removeAttr("data-checked-children-quantity");
+        withCheckedChildren.removeClass("checked-children");
+        $(".find-fellowships .checked-children").removeClass("checked-children");
+        var checkedFieldsets = $(".find-fellowships fieldset:has(input:checked)");
+        checkedFieldsets.each(function() {
+          $(this).addClass("checked-children");
+          $(this).find("legend").attr("data-checked-children-quantity",$(this).find("input:checked").length);
+	});
+      }
+
+      // Honestly am not quite sure why I can do ajaxStart once, but adding once() to ajaxStop doesn't make things work.
+      // Or why I really even need ajaxStop in the first place.
+      // Probably could use some improvement, but it's at least working for now.
+      $(document, context).once().ajaxStart(checkedChildren);
+      $(document, context).ajaxStop(checkedChildren);
+
     }
   }
 })(jQuery);
