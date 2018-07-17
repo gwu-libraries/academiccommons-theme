f72fc81ce2343b39f6e496b5772cc26d6cff3c99
Max Starkenburg <maxstarkenburg@gmail.com>
2018-07-17 11:28:04 -0400

    Take into account the possibility that somebody has the admin toolbar at left instead of at top.  Still not perfect in every single situation/size/resize/etc., but such perfection will have to wait.

--- lai/js/build/sticky-nav.js
+++ lai/js/build/sticky-nav.js
@@ -4,7 +4,7 @@
     // Fixed position menus after scrolling down past their location on the page
     if ($(window).width() > 640) {
       var toolbarTabHeight = $("#toolbar-administration .toolbar-tab:first").outerHeight() || 0;
-      var toolbarTrayHeight = $("#toolbar-administration .toolbar-tray:first").outerHeight() || 0;
+      var toolbarTrayHeight = $(".toolbar-horizontal #toolbar-administration .toolbar-tray:first").outerHeight() || 0;
       var toolbarHeight = toolbarTabHeight + toolbarTrayHeight; // for some reason $("#toolbar-administration").outerHeight() is always returning 0 for me
       var navBarOffset = navBar.offset().top;
       $(document).on("scroll", function() {
