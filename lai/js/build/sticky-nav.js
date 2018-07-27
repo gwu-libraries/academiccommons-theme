a8c6f2e88e126f508e59ef61a3b3cee87c0d6814
Max Starkenburg <maxstarkenburg@gmail.com>
2018-07-27 14:56:42 -0400

    Add a margin on the body when navbar goes sticky so that the transition is less jumpy and is less likely to result in an infinite back-and-forth strobe-like jump-fest.

--- lai/js/build/sticky-nav.js
+++ lai/js/build/sticky-nav.js
@@ -1,6 +1,7 @@
 (function ($) {
   $(document).ready(function(event) {
     var navBar = $("#nav-bar");
+    var body = $("body");
     // Fixed position menus after scrolling down past their location on the page
     if ($(window).width() > 640) {
       var toolbarTabHeight = $("#toolbar-administration .toolbar-tab:first").outerHeight() || 0;
@@ -13,8 +14,10 @@
           navBar.css({
             "top" : toolbarHeight + "px",
           });
+          body.css("margin-top",navBar.outerHeight()); // margin instead of padding since Drupal is already using padding to account for the admin toolbar
         } else {
           navBar.removeClass("sticky");
+          body.css("margin-top","0");
         }
       });
     }
