147e372fc34b578a7a2f2f028000693e20b8a44d
Max Starkenburg <maxstarkenburg@gmail.com>
2018-07-27 17:38:15 -0400

    Trying a different way to avoid the infinitely jumpy page problem that I was still coming across despite my last attempt at a fix.

--- lai/js/build/sticky-nav.js
+++ lai/js/build/sticky-nav.js
@@ -1,23 +1,21 @@
 (function ($) {
   $(document).ready(function(event) {
     var navBar = $("#nav-bar");
-    var body = $("body");
     // Fixed position menus after scrolling down past their location on the page
     if ($(window).width() > 640) {
       var toolbarTabHeight = $("#toolbar-administration .toolbar-tab:first").outerHeight() || 0;
       var toolbarTrayHeight = $(".toolbar-horizontal #toolbar-administration .toolbar-tray:first").outerHeight() || 0;
       var toolbarHeight = toolbarTabHeight + toolbarTrayHeight; // for some reason $("#toolbar-administration").outerHeight() is always returning 0 for me
       var navBarOffset = navBar.offset().top;
+      navBar.parent().css("min-height", navBar.outerHeight()); // to prevent jumpiness on pages that are of a certain height
       $(document).on("scroll", function() {
         if ($(document).scrollTop() > navBarOffset - toolbarHeight) {
           navBar.addClass("sticky");
           navBar.css({
             "top" : toolbarHeight + "px",
           });
-          body.css("margin-top",navBar.outerHeight()); // margin instead of padding since Drupal is already using padding to account for the admin toolbar
         } else {
           navBar.removeClass("sticky");
-          body.css("margin-top","0");
         }
       });
     }
