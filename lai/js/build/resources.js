2bd284fcd6a3d352529cf1e7492995a5997e2fe7
Max Starkenburg <maxstarkenburg@gmail.com>
2018-07-30 02:35:13 -0400

    Change Drupal.behaviors to just regular document.ready for the moment to avoid the problem with Drupal not being recognized while logged out.  Also disable tabbing to the <a> tags after ajax was loaded too (since for some reason that view is loading via ajax in spite of its settings).

--- lai/js/build/resources.js
+++ lai/js/build/resources.js
@@ -1,14 +1,19 @@
 (function ($) {
-  Drupal.behaviors.resources = {
-    attach: function(context, settings) {
-      $(".resource-bucket a", context).attr("tabindex","-1");
-      $("#main-content", context).on("click keypress", ".resource-bucket", function(e) {
+//  Drupal.behaviors.resources = {
+//    attach: function(context, settings) {
+//      $(".resource-bucket a", context).attr("tabindex","-1");
+//      $("#main-content", context).on("click keypress", ".resource-bucket", function(e) {
+      $(".resource-bucket a").attr("tabindex","-1");
+      $(document).ajaxComplete(function(){
+        $(".resource-bucket a").attr("tabindex","-1");
+      });
+      $("#main-content").on("click keypress", ".resource-bucket", function(e) {
         if (e.type == "click" || (e.type == "keypress" && (e.which == 13 || e.which == 10))) {
           var link = $(this).find("h2 a")[0].href;
           var windowName = e.ctrlKey == true ? "_blank" : "_self";
           window.open(link, windowName);
         }
       });
-    }
-  }
+//    }
+//  }
 })(jQuery);
