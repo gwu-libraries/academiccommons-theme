793fb7522ca0338f571522c09c37ece3ad718349
Max Starkenburg <maxstarkenburg@gmail.com>
2019-02-04 13:19:26 -0500

    Add some code that normally hides the new PC RSVP/Appointment field (machine name: field_review_session_info), but unhides it and hides everything else if displayed from an iframe (abusing the Calendly popup that we currently support). Fixes #44.

--- lai/js/build/peer-coaching.js
+++ lai/js/build/peer-coaching.js
@@ -1,8 +1,22 @@
 (function ($) {
 
   // This makes the Calendly link pop open in the same window since Views strips out onclick attributes
-  $('.peer-coaching-detail a[href*="calendly"]').each(function() {
+  // It used to use `a[href*="calendly"]` but we also want to hack something together for non-Calendly links (see below)
+  $('.peer-coaching-detail a:first').each(function() {
     $(this).attr("onclick","Calendly.showPopupWidget('" + $(this).attr("href") + "');return false;");
   });
+
+  // Hack to reuse popup intended for Calendly links to now also show the value of the "Review Session Info" field
+  if (window.self != window.top) {
+    // If inside an iframe, hide elements like headers and footers that make for possible infinite nesting and linking from within the iframe we'd rather avoid
+    $(".peer_coaching_appointment_rsvp").on("load", "#toolbar-administration, .region-header, #block-lai-page-title, #block-lai-local-tasks, #block-views-block-peer-coaching-detail-block-1, .region-footer, #drupal-live-announce, #block-academiccommonsfooter", function() {
+      // doing as delegated .on("load") instead of .find() since #block-academiccommonsfooter (at least) doesn't seem to load at same time as the rest of the page
+      $(this).hide();
+    });
+    // Though do turn on the field we normally hide in display if somebody lands on the node from search
+    $(".peer_coaching_appointment_rsvp .peer_coaching_appointment_rsvp__review-session-info").show();
+    // Make sure any links within the field, such as to other GW departments, open in a new tab instead of possibly within the iframe
+    $(".peer_coaching_appointment_rsvp #content a").attr("target","_blank");
+  }
 
 })(jQuery);
